da5184abf8a93121ed330bcf17767ec971421fe7
Вячеслав Тихановский <vti@cpan.org>
2009-07-22 00:58:23 +0000

    Import of VTI/Protocol-XMLRPC-0.04 from CPAN.
    gitpan-cpan-distribution: Protocol-XMLRPC
    gitpan-cpan-version:      0.04
    gitpan-cpan-path:         VTI/Protocol-XMLRPC-0.04.tar.gz
    gitpan-cpan-author:       VTI
    gitpan-cpan-maturity:     released

--- lib/Protocol/XMLRPC.pm
+++ lib/Protocol/XMLRPC.pm
@@ -4,7 +4,7 @@ use Any::Moose;
 use Protocol::XMLRPC::MethodCall;
 use Protocol::XMLRPC::MethodResponse;
 
-our $VERSION = '0.03';
+our $VERSION = '0.04';
 
 has http_req_cb => (
     required => 1,
